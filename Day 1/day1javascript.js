caa9e8e4b6f537264619ea9f36685098c8b780a6
zdickerson <zachary-dickerson@uiowa.edu>
2024-01-22 22:03:51 -0600

    Completion of Day 1
    Completion of day 1 goals for python and javascript. Javascript file needs to be ran in a browser.

---
+++ Day 1/day1javascript.js
@@ -0,0 +1,8 @@
+
+// Note this has to be ran in a browser window.
+
+console.log("Hello, World!");
+console.log("Welcome to the Band Name Generator.")
+const myCity = prompt("What's name of the city you grew up?\n")
+const myPet = prompt("What's your pet's name?\n")
+console.log("Your band name could be ${myCity} ${myPet}")
---
+++ Day 1/day1python.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+myCity = input("What's name of the city you grew up?\n")
+myPet = input("What's your pet's name?\n")
+print(f"Your band name could be {myCity} {myPet}")
