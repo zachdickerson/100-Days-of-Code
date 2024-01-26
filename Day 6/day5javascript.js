f9805aabe2f8d6d7d0e3263d21e3f05c1629eeb7
zdickerson <zachary-dickerson@uiowa.edu>
2024-01-26 15:50:02 -0600

    Completion of day 6
    Completion of Day 6, no code on here it was done on reborgs world.

---
+++ Day 6/day5javascript.js
@@ -0,0 +1,7 @@
+/* 
+
+Functions
+
+Learned on reborgs world
+
+*/
---
+++ Day 6/day5python.py
@@ -0,0 +1,19 @@
+''''Functions
+
+learn about loops and while loop all on reborgs world.com
+
+
+'''
+
+print("hello")
+num = len("hello")
+print(num)
+
+
+def my_function():
+    print('hello')
+    print('hi')
+
+my_function()
+
+
