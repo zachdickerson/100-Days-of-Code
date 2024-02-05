3630c0d17e148328d6435dd619b4723af4646af3
zdickerson <zachary-dickerson@uiowa.edu>
2024-02-05 14:26:06 -0600

    Completion of Day 10 and Capstone
    Completion of Day 10 and Day 11 Capstone. Day 10 was the creation of a calculator, and Day 11 was a capstone blackjack game. Only need to do both in Javascript to close out the days.

--- Day 10/python/calculator.py
+++ Day 10/python/calculator.py
@@ -1,5 +1,4 @@
-
-
+import art
 
 #Calculator
 
@@ -26,5 +25,32 @@ operations = {
     '/':divide,
 }
 
-num1 = int(input("What's the first number?: "))
-num1 = int(input("What's the second number?: "))
+def calculator():
+
+    print(art.logo)
+
+    num1 = float(input("What's the first number?: "))
+    for i in operations:
+        print(i)
+
+    should_continue = True
+
+    while should_continue:
+        
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1,num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        checker = input(f"Type 'y' to coninue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if checker == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
+
---
+++ Day 11 - Capstone Project 1/python/blackjack.py
@@ -0,0 +1,105 @@
+import art
+import random
+
+############### Blackjack Project #####################
+
+#Difficulty Normal 😎: Use all Hints below to complete the project.
+#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
+#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
+#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def get_computer_card():
+    computer_cards = []
+
+    computer = 0
+
+    while computer <= 21:
+        x = cards[random.randint(0,12)]
+        computer += x
+        computer_cards.append(x)
+    
+    if sum(computer_cards) > 21:
+        #print(computer_cards)
+        computer_cards.pop()
+
+    return computer_cards
+
+
+def get_human_cards():
+    human_cards = []
+    
+    for i in range(0,2):
+        x = cards[random.randint(0,12)]
+        human_cards.append(x)
+    return human_cards
+
+
+def blackjack():
+
+    computer_cards = get_computer_card()
+    computer_sum = sum(computer_cards)
+
+    human_cards = get_human_cards()
+
+    print(art.logo)
+    print(f"Your cards: {human_cards}")
+    print(f"Computer's first card: {computer_cards[0]}")
+    # print(computer_cards)
+
+    hitting = True
+    while hitting:
+        another_hit = input("Type 'y' to get another card, type 'n' to pass: ")
+
+        if another_hit == 'y':
+            human_cards.append(cards[random.randint(0,12)])
+            print(f'Your cards {human_cards} and the total is {sum(human_cards)}')
+            if sum(human_cards) > 21:
+                print(f"Your final hand: {human_cards}")
+                print(f"Computers final hand: {computer_cards}")
+                print('Computer Wins!')
+                hitting = False
+            elif sum(human_cards) == 21 and computer_sum == 21:
+                print(f"Your final hand: {human_cards}")
+                print(f"Computers final hand: {computer_cards}")
+                print("It's a draw!")
+                hitting = False
+        else:
+            hitting = False
+            if sum(human_cards) == computer_sum:
+                print(f"Your final hand: {human_cards}")
+                print(f"Computers final hand: {computer_cards}")
+                print("It's a draw!")
+            elif sum(human_cards) > computer_sum:
+                print(f"Your final hand: {human_cards}")
+                print(f"Computers final hand: {computer_cards}")
+                print("You win!")
+            else:
+                print(f"Your final hand: {human_cards}")
+                print(f"Computers final hand: {computer_cards}")
+                print('Computer wins!')
+
+    game2 = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+    if game2 == 'y':
+        blackjack()
+    else:
+        print("Thank you for playing!")
+
+if input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == 'y':
+    blackjack()
+
+
+# I completed the problem with only 1 hint! Which is expert ;)
