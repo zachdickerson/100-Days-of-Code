3023385378bfd0636e72c7c29de5c10d41a24ad8
zdickerson <zachary-dickerson@uiowa.edu>
2024-02-09 16:50:51 -0600

    Completion of Day 13
    Completion of Day 13 problems

---
+++ Day 13/python/debuggingOddOrEven.py
@@ -0,0 +1,7 @@
+# Which number do you want to check?
+number = int(input())
+
+if number % 2 == 0:  # Fixed the equals to double equals
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
